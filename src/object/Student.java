2bab88f9f028749a3964c8871e3bf66026133631
code <nguyenuyen.dt@gmail.com>
2021-10-16 14:33:57 +0700

    CODETESTPUSH

---
+++ src/object/Student.java
@@ -0,0 +1,24 @@
+package object;
+
+public class Student {
+	// tao thuoc tinh
+	private int id;
+	private String name;
+	private int age;
+	
+	
+// ham tao
+	public Student (String name)
+	{
+		System.out.println("name of student : " + name);
+	}
+	
+	
+	
+	public static void main (String[] args)
+	{
+		 
+		Student student = new Student("Nguyen Van A");
+		
+	}
+}
